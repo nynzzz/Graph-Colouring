d634cd05c315b5c977862ba9023b47ad18fcf059
Pingu22981 <115260150+Pingu22981@users.noreply.github.com>
2022-11-29 12:30:24 +0100

    I hope this doesn't break anything

--- src/Phase_2/RandomGraph.java
+++ src/Phase_2/RandomGraph.java
@@ -1,4 +1,33 @@
 package Phase_2;
 
+import java.util.Random;
 public class RandomGraph {
+
+    public static void generateRandom(int vertices, int edges){
+        int graph[][] = new int [vertices][vertices];
+        int edgeCount=0;
+        while(edgeCount<=edges)
+        {
+            Random rand = new Random();
+            int ran1 = rand.nextInt(vertices  );
+            int ran2=  rand.nextInt(vertices );
+
+            if(graph[ran1][ran2]==0){
+                graph[ran1][ran2] = 1;
+            }
+            else if(graph[ran1][ran2]==1){
+                edgeCount--;
+            }
+            edgeCount++;
+        }
+
+        for (int i = 0; i<vertices; i++) {
+            for (int j = 0; j<vertices; j++) {
+                System.out.print(graph[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+
+    }
 }
